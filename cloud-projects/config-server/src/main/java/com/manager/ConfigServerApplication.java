0841dde86be32759d5e33fd2c4cb419c628baf1d
xiachao616345713 <616345713@qq.com>
2018-04-12 10:40:03 +0800

    modify project structure

--- cloud-projects/config-server/src/main/java/com/manager/ConfigServerApplication.java
+++ cloud-projects/config-server/src/main/java/com/manager/ConfigServerApplication.java
@@ -1,4 +1,4 @@
-package com.manager.config;
+package com.manager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,9 +8,9 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @SpringBootApplication
 @EnableConfigServer
 @EnableEurekaClient
-public class ConfigApplication {
+public class ConfigServerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ConfigApplication.class, args);
+        SpringApplication.run(ConfigServerApplication.class, args);
     }
 }
--- cloud-projects/eureka-server/src/main/java/com/manager/EurekaServerApplication.java
+++ cloud-projects/eureka-server/src/main/java/com/manager/EurekaServerApplication.java
@@ -1,4 +1,4 @@
-package com.manager.server;
+package com.manager;
 
 
 import org.springframework.boot.SpringApplication;
@@ -7,9 +7,9 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class ServerApplication {
+public class EurekaServerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ServerApplication.class, args);
+        SpringApplication.run(EurekaServerApplication.class, args);
     }
 }
